86ad4c61bd5d9edc1f2ecc24e3dbff56baa40432
NguyenKhacNgoc <khacngoc6969@gmail.com>
2023-10-21 13:34:14 +0700

    chat nhung chua co realtime

--- ecommerce/src/main/java/com/server/ecommerce/Controller/MessageController.java
+++ ecommerce/src/main/java/com/server/ecommerce/Controller/MessageController.java
@@ -1,8 +1,9 @@
 package com.server.ecommerce.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.server.ecommerce.DTO.MessageDTO;
+import com.server.ecommerce.DTO.MessageResponseDTO;
+
+import com.server.ecommerce.DTO.ProfileMessageResponseDTO;
 import com.server.ecommerce.Entity.Message;
+import com.server.ecommerce.Entity.Profile;
 import com.server.ecommerce.JWT.JwtTokenUtil;
 import com.server.ecommerce.Respository.MessageRespository;
+import com.server.ecommerce.Respository.ProfileRespository;
 import com.server.ecommerce.Respository.UserRespository;
 
 @RestController
@@ -28,8 +34,10 @@ public class MessageController {
     private JwtTokenUtil jwtTokenUtil;
     @Autowired
     private UserRespository userRespository;
+    @Autowired
+    private ProfileRespository profileRespository;
 
-    @PostMapping("/send")
+    @PostMapping("/chat/send")
     public ResponseEntity<?> sendMessage(@RequestHeader("Authorization") String authorization,
             @RequestBody MessageDTO request) {
         String token = authorization.substring(7);
@@ -48,7 +56,7 @@ public class MessageController {
 
     }
 
-    @GetMapping("/history")
+    @GetMapping("/chat/history")
     public ResponseEntity<?> getHistory(@RequestHeader("Authorization") String authorization,
             @RequestParam("receiverID") Long receiverID) {
         String token = authorization.substring(7);
@@ -56,9 +64,58 @@ public class MessageController {
         if (jwtTokenUtil.validateToken(token)) {
             String email = jwtTokenUtil.getEmailFromToken(token);
             Long senderID = userRespository.findByEmail(email).get().getId();
-            List<Message> messages = messageRespository.findAllBySenderIDAndReceiverIDOrReceiverIDAndSenderID(senderID,
-                    receiverID, receiverID, senderID);
-            return ResponseEntity.ok(messages);
+            List<Message> messages = messageRespository.findBySenderIDAndReceiverIDOrSenderIDAndReceiverIDOrderBySendAt(
+                    senderID, receiverID, receiverID, senderID);
+            List<MessageResponseDTO> messageResponseDTOs = new ArrayList<>();
+            for (Message message : messages) {
+                MessageResponseDTO messageResponseDTO = new MessageResponseDTO();
+                messageResponseDTO.setMessage(message);
+                if (message.getSenderID() == senderID)
+                    messageResponseDTO.setStatus("send");
+                else
+                    messageResponseDTO.setStatus("receiver");
+
+                messageResponseDTOs.add(messageResponseDTO);
+            }
+            return ResponseEntity.ok(messageResponseDTOs);
+        } else
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @GetMapping("/getMessageByUserID")
+    public ResponseEntity<?> getMessageByUserID(@RequestHeader("Authorization") String authorization) {
+        String token = authorization.substring(7);
+        if (jwtTokenUtil.validateToken(token)) {
+            Long userID = userRespository.findByEmail(jwtTokenUtil.getEmailFromToken(token)).get().getId();
+            List<Long> list = messageRespository.getMessageByUserID(userID);
+            List<ProfileMessageResponseDTO> profileMessageResponseDTOs = new ArrayList<>();
+            for (Long id : list) {
+                Profile profile = profileRespository.findById(id).get();
+                ProfileMessageResponseDTO profileMessageResponseDTO = new ProfileMessageResponseDTO();
+                profileMessageResponseDTO.setId(profile.getId());
+                profileMessageResponseDTO.setPhoneNumber(profile.getPhoneNumber());
+                profileMessageResponseDTO.setAddress(profile.getAddress());
+                profileMessageResponseDTO.setDateofbirth(profile.getDateofbirth());
+                profileMessageResponseDTO.setEmail(profile.getUser().getEmail());
+                profileMessageResponseDTO.setFullName(profile.getFullName());
+                profileMessageResponseDTO.setSex(profile.getSex());
+
+                Message message = messageRespository.findLastMessageBetweenUser(profile.getId(), userID).get(0);
+                MessageDTO messageDTO = new MessageDTO();
+                messageDTO.setContent(message.getContent());
+                messageDTO.setId(message.getId());
+                messageDTO.setReceiverID(message.getReceiverID());
+                messageDTO.setSendAt(message.getSendAt());
+                messageDTO.setSenderID(message.getSenderID());
+                profileMessageResponseDTO.setMessageDTO(messageDTO);
+                if (userID == message.getSenderID())
+                    profileMessageResponseDTO.setStatus("send");
+                else
+                    profileMessageResponseDTO.setStatus("receiver");
+                profileMessageResponseDTOs.add(profileMessageResponseDTO);
+
+            }
+            return ResponseEntity.ok(profileMessageResponseDTOs);
         } else
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
---
+++ ecommerce/src/main/java/com/server/ecommerce/DTO/ProfileMessageResponseDTO.java
@@ -0,0 +1,26 @@
+package com.server.ecommerce.DTO;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileMessageResponseDTO {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+
+    private String address;
+
+    private String sex;
+
+    private Date dateofbirth;
+    private MessageDTO messageDTO;
+    private String status;
+
+}
